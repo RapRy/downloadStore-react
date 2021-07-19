c74ebe54375775c09fc3172598bb6f93bc2b2530
Ryan <ry.quickcell@gmail.com>
2021-07-19 18:13:38 +0800

    code formatted

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-import React, { lazy, Suspense } from 'react'
-import { createTheme, ThemeProvider, CssBaseline } from '@material-ui/core'
+import React, { lazy, Suspense } from "react";
+import { createTheme, ThemeProvider, CssBaseline } from "@material-ui/core";
 
-const Auth = lazy(() => import('./components/Auth/Auth'))
+const Auth = lazy(() => import("./components/Auth/Auth"));
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: 'rgba(69, 217, 226, 1)',
-      light: 'rgba(142, 255, 102, 1)',
-      dark: 'rgba(32, 124, 232, 1)',
+      main: "rgba(69, 217, 226, 1)",
+      light: "rgba(142, 255, 102, 1)",
+      dark: "rgba(32, 124, 232, 1)",
     },
     secondary: {
-      main: 'rgba(226, 69, 69, 1)',
-      light: 'rgba(255, 102, 102)'
+      main: "rgba(226, 69, 69, 1)",
+      light: "rgba(255, 102, 102)",
     },
     neutrals: {
-      main: 'rgba(224, 224, 224, 1)',
-      light: 'rgba(242, 242, 242, 1)',
-      dark: 'rgba(64, 58, 58, 1)',
-      contrastText: 'rgba(0, 0, 0, 1)'
-    }
+      main: "rgba(224, 224, 224, 1)",
+      light: "rgba(242, 242, 242, 1)",
+      dark: "rgba(64, 58, 58, 1)",
+      contrastText: "rgba(0, 0, 0, 1)",
+    },
   },
   shadows: [
-    'none',
-    '0px 0px 10px rgba(0, 0, 0, .25)',
-    '2px 2px 4px rgba(142, 255, 102,.25)',
-    '2px 2px 4px rgba(32, 124, 232,.25)'
+    "none",
+    "0px 0px 10px rgba(0, 0, 0, .25)",
+    "2px 2px 4px rgba(142, 255, 102,.25)",
+    "2px 2px 4px rgba(32, 124, 232,.25)",
   ],
   shape: {
     borderRadiusFive: 5,
-    borderRadiusTen: 10
-  }
-})
+    borderRadiusTen: 10,
+  },
+});
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Suspense fallback={`<p>loading...</p>`}>
-          <Auth />
+        <Auth />
       </Suspense>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,44 +1,80 @@
-import React, { useState, useCallback } from 'react'
-import { Container, makeStyles, Typography } from '@material-ui/core'
+import React, { useState, useCallback } from "react";
+import { Container, makeStyles, Typography } from "@material-ui/core";
 
-import HeadBg from '../GlobalComponents/Backgrounds/HeadBg'
-import InputFields from '../GlobalComponents/Forms/InputFields'
+import HeadBg from "../GlobalComponents/Backgrounds/HeadBg";
+import InputFields from "../GlobalComponents/Forms/InputFields";
 
-const initialData = { mobile: "", firstName: "", lastName: "", email: "", password: "", confirmPassword: "" }
+const initialData = {
+  mobile: "",
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
-const initialErrors = { mobile: "field is required", firstName: "", lastName: "", email: "", password: "", confirmPassword: "" }
+const initialErrors = {
+  mobile: "field is required",
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const Auth = () => {
-    const classes = useStyles()
-    const [formData, setFormData] = useState(initialData)
-    const [erros, setErrors] = useState(initialErrors)
+  const classes = useStyles();
+  const [formData, setFormData] = useState(initialData);
+  const [erros, setErrors] = useState(initialErrors);
 
-    const inputChange = useCallback((e) => {
-        if(e.target.name === "mobile"){
-            if(isNaN(e.target.value)){
-                return
-            }
+  const inputChange = useCallback(
+    (e) => {
+      if (e.target.name === "mobile") {
+        if (isNaN(e.target.value)) {
+          return;
         }
-        // setFormData({ ...formData, [e.target.name]: e.target.value })
-        setFormData(prevState => {
-            return { ...prevState, [e.target.name]: e.target.value }
-        })
-    }, [formData])
+      }
+      // setFormData({ ...formData, [e.target.name]: e.target.value })
+      setFormData((prevState) => {
+        return { ...prevState, [e.target.name]: e.target.value };
+      });
+    },
+    [formData]
+  );
 
-    return (
-        <Container>
-            <form>
-                <InputFields value={formData.mobile} type="text" name="mobile" label="Mobile Number:" errors={erros} inputChange={inputChange} />
-                <InputFields value={formData.password} type="password" name="password" label="Password:" errors={erros} inputChange={inputChange} />
-                <InputFields value={formData.email} type="email" name="email" label="Email Address:" errors={erros} inputChange={inputChange} />
-            </form>
-            {/* <HeadBg /> */}
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <form>
+        <InputFields
+          value={formData.mobile}
+          type="text"
+          name="mobile"
+          label="Mobile Number:"
+          errors={erros}
+          inputChange={inputChange}
+        />
+        <InputFields
+          value={formData.password}
+          type="password"
+          name="password"
+          label="Password:"
+          errors={erros}
+          inputChange={inputChange}
+        />
+        <InputFields
+          value={formData.email}
+          type="email"
+          name="email"
+          label="Email Address:"
+          errors={erros}
+          inputChange={inputChange}
+        />
+      </form>
+      {/* <HeadBg /> */}
+    </Container>
+  );
+};
 
-const useStyles = makeStyles({
+const useStyles = makeStyles({});
 
-})
-
-export default Auth
+export default Auth;
